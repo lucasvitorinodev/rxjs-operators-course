dd8e6cca06278ec7be190864942701a58e427cc0
lvitorino <lucas.vitorino@nelnet.net>
2023-05-22 10:00:43 -0500

    feat(observer): add observer example and definition

--- src/app/components/observables/observables.component.ts
+++ src/app/components/observables/observables.component.ts
@@ -11,6 +11,11 @@ import { Observable } from 'rxjs';
 // An observable can be seen as a stream of data that emits values at different points in time.
 // These values can be of any type, such as numbers, strings, objects, or even other observables.
 // Observables are used to model and manipulate data flows in a reactive manner.
+
+// 2. Observer
+// In RxJS, an observer is an object that defines how to handle the values, errors, and completion signals emitted by
+// an observable. It is a core component in the observer pattern and is used to consume and react to the data
+// emitted by an observable.
 export class ObservablesComponent implements OnInit {
   ngOnInit() {
     this.initObservable();
@@ -25,10 +30,12 @@ export class ObservablesComponent implements OnInit {
       subscriber.complete();
     });
 
-    observable.subscribe(
-      (value) => console.log(value),
-      (err) => console.log(err),
-      () => console.log('Completed')
-    );
+    const observer = {
+      next: (value: any) => console.log(`Observer next = ${value}`),
+      error: (error: any) => console.error(`Observer error = ${error}`),
+      complete: () => console.log('Observer completed!'),
+    };
+
+    observable.subscribe(observer);
   }
 }
