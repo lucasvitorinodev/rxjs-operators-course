11631c455b42f2bc9c1c5e8f9627f09634685ce3
lvitorino <lucas.vitorino@nelnet.net>
2023-05-22 19:48:29 -0500

    feat(subject/behavior subject): add subject/behavior subject example and definition

---
+++ src/app/components/subject/subject.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-subject',
+  templateUrl: './subject.component.html',
+  styleUrls: ['./subject.component.scss'],
+})
+
+// 4. Subject
+// In RxJS, a Subject is a special type of observable that allows values to be multicasted to multiple observers.
+// It acts both as an Observable and an Observer, which means it can emit values and also be subscribed to like any
+// other observable.
+//
+// Subjects are useful when you want to share the same set of values with multiple observers.
+
+// 5. Behavior Subject
+// In RxJS, a BehaviorSubject is a variant of the Subject class that retains the most recent value
+// emitted and replays it to new subscribers. It is both an Observable and an Observer, just like a Subject,
+// but it has an initial value and remembers the latest emitted value.
+export class SubjectComponent implements OnInit {
+  ngOnInit(): void {
+    this.initSubject();
+  }
+
+  initSubject() {
+    const subject = new Subject<number>();
+    const behaviorSubject = new BehaviorSubject<number>(0);
+
+    subject.subscribe({
+      next: (value: number) => console.log(`Subs 1 = ${value}`),
+    });
+
+    subject.subscribe({
+      next: (value: number) => console.log(`Subs 2 = ${value}`),
+    });
+
+    behaviorSubject.subscribe({
+      next: (value: number) => console.log(`B Subs 1 = ${value}`),
+    });
+
+    behaviorSubject.subscribe({
+      next: (value: number) => console.log(`B Subs 2 = ${value}`),
+    });
+
+    subject.next(10);
+    subject.next(12);
+    behaviorSubject.next(12);
+  }
+}
